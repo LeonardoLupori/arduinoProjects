914302f07842d3335609522b1e014419fbee513a
Leonardo <leonardo.lupori@gmail.com>
2021-11-14 19:42:33 +0100

    update inputParser

--- foodCuePreference/foodCuePreference/inputParser.ino
+++ foodCuePreference/foodCuePreference/inputParser.ino
@@ -4,10 +4,13 @@
  */
 
 
-void checkForSerialInputs(){
+char checkForSerialInputs(bool echoing){
   if(Serial.available() > 0){
     char message = Serial.read();
-    parseMessage(message);
+    response = parseMessage(message);
+    if(echoing){
+      return response;
+    }
   }
 }
 
@@ -16,20 +19,26 @@ void checkForSerialInputs(){
 // Function for PARSING the serial inputs from Python
 // --------------------------------------------------------------
 
-void parseMessage(char msg){
+char parseMessage(char msg){
   switch (msg) {
   case 'T':         // Start a new CONDITIONAL REWARD TRIAL
     trial_condReward();
+    return 'T';
     break;
   case 'P':         // Start a new PAVLOVIAN TRIAL
+    return 'P';
     break;
   case 'R':         // Deliver REWARD once
+    return 'R';
     break;
   case 'Q':         // Deliver AVERSIVE (quinine) once
+    return 'Q';
     break;
   case 'L':         // Fast FORWARD of the syringe pump
+    return 'L';
     break;
   case 'J':         // Fast BACKWARD of the syringe pump
+    return 'J';
     break;
   default:          // if nothing else matches, do the default
     break;
